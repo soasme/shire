0c0c60722014ce8f43e2a6da4cf372c96720920f
soasme <soasme@gmail.com>
2019-10-22 22:56:14 +1300

    rename server to {id}.{region}.svc.marksth.fun.

--- infra/main.tf
+++ infra/main.tf
@@ -6,8 +6,8 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-resource "digitalocean_droplet" "fun0001" {
-  name      = "fun0001"
+resource "digitalocean_droplet" "server0001" {
+  name      = "0001.sfo2.svc.marksth.fun"
   size      = "s-1vcpu-1gb"
   image     = "centos-7-x64"
   region    = "sfo2"
