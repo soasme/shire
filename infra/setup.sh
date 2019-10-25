e04d593c918c55136206c56d51f064a67921f45e
soasme <soasme@gmail.com>
2019-10-25 19:06:55 +1300

    add site domain.

--- infra/setup.sh
+++ infra/setup.sh
@@ -3,5 +3,6 @@
 export `dotenv list`
 terraform apply -auto-approve
 ansible-playbook sys/base-provision.yml
-ansible-playbook sys/cert-provision.yml
+ansible-playbook sys/prom-provision.yml
 ansible-playbook sys/lb-provision.yml
+ansible-playbook sys/db-provision.yml
