97aa092b0a411a243e037f1701542d0b58a392a6
soasme <soasme@gmail.com>
2019-10-26 00:09:36 +1300

    add todo.

--- infra/setup.sh
+++ infra/setup.sh
@@ -6,3 +6,4 @@ ansible-playbook sys/base-provision.yml
 ansible-playbook sys/prom-provision.yml
 ansible-playbook sys/lb-provision.yml
 ansible-playbook sys/db-provision.yml
+ansible-playbook sys/web-provision.yml
