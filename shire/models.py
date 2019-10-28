c54a5f50a434d82d441b23c067f4b43dc0d5f61f
soasme <soasme@gmail.com>
2019-10-28 23:31:46 +1300

    typo fixed.

--- shire/models.py
+++ shire/models.py
@@ -111,7 +111,7 @@ class Thing(db.Model):
         base_query = Thing.query.filter_by(user_id=user_id).filter(Thing.tags.contains(tags))
         if not include_private:
             base_query = base_query.filter_by(shared=True)
-        return base_query.order_by(cls.time.desc()).offset(offset).limit(limit).all())
+        return base_query.order_by(cls.time.desc()).offset(offset).limit(limit).all()
 
     @classmethod
     def get_user_things_cnt(cls, user_id):
