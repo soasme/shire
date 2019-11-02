0010e07977b962e38e1a1c7c26e2ed320f5b5f2b
soasme <soasme@gmail.com>
2019-11-02 23:04:14 +1300

    add recovery doc.

--- infra/backup.sh
+++ infra/backup.sh
@@ -7,22 +7,22 @@
 
 set -e
 
-echo "backup env"
+echo "Backup env"
 cp ./.env ${BACKUP_DIR}/env
-echo "done"
+echo "Done. Recover by copying back to ./.env"
 
-echo "backup tfstates"
+echo "Backup tfstates"
 cp ./terraform.tfstate ${BACKUP_DIR}/terraform.tfstate
-echo "done"
+echo "Done. Recover by copying back to ./terraform.tfstate"
 
 # backup certificates
 LBHOST=`terraform output server_00001_ipv4_address`
 
-echo "backup certificates"
+echo "Backup certificates"
 scp -r root@$LBHOST:/etc/letsencrypt ${BACKUP_DIR}/etc
-echo "done"
+echo 'Done. Recover by running `scp -r ${BACKUP_DIR}/etc/letsencrypt root@$LBHOST:/etc`'
 
 # backup database
-echo "backup database"
+echo "Backup database"
 ssh root@$LBHOST PGPASSWORD=${SHIRE_DATABASE_PASS} sudo -E -u postgres /usr/pgsql-12/bin/pg_dump -Fc --no-acl --no-owner -h localhost -U ${SHIRE_DATABASE_USER} ${SHIRE_DATABASE_NAME} > /tmp/latest.dump
-echo "done"
+echo 'Done. Recover by scping data back to server and running `sudo -u postgres /usr/pgsql-12/bin/pg_restore --verbose --clean --no-owner --no-acl -h localhost -p 5432 -U $PG_USER -d $PG_DATABASE -Fc -1 /tmp/latest.dump`'
