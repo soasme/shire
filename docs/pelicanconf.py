6079a296acf09b90f10beea863f5129ad428718a
soasme <soasme@gmail.com>
2019-11-18 23:03:43 +1300

    upload blog to gh-pages.

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -31,3 +31,6 @@ DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
