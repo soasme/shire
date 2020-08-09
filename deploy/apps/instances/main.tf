02dc2d410040ed42de450875d6ba3b2692a88380
soasme <soasme@gmail.com>
2020-08-09 21:48:44 +1200

    add app instances variables.

--- deploy/apps/instances/main.tf
+++ deploy/apps/instances/main.tf
@@ -1,21 +1,29 @@
+provider "digitalocean" {}
+
+data "digitalocean_vpc" "this" {
+  id = var.vpc_id
+}
+
 resource "digitalocean_droplet" "app" {
   count              = var.app_instances_count
+
   name               = join("-", [var.app_instances_prefix, format("%04d", count.index)])
-  size               = var.app_size
-  image              = var.app_image
-  tags               = ["app"]
-  region             = var.region
+  size               = var.app_instances_size
+  image              = var.app_instances_image
+  tags               = var.app_instances_tags
+  region             = data.digitalocean_vpc.this.region
   ssh_keys           = var.ssh_keys
   private_networking = true
 }
 
 resource "digitalocean_droplet" "db" {
   count              = var.db_instances_count
+
   name               = join("-", [var.db_instances_prefix, format("%04d", count.index)])
-  size               = var.db_size
-  image              = var.db_image
-  tags               = ["db"]
-  region             = var.region
+  size               = var.db_instances_size
+  image              = var.db_instances_image
+  tags               = var.db_instances_tags
+  region             = data.digitalocean_vpc.this.region
   ssh_keys           = var.ssh_keys
   private_networking = true
 }
---
+++ deploy/apps/instances/variables.tf
@@ -0,0 +1,71 @@
+variable "vpc_id" {
+  type        = string
+  default     = null
+  description = "The id of digitalocean VPC."
+}
+
+variable "app_instances_count" {
+  type        = number
+  default     = 0
+  description = "The total number of instances tagged by app."
+}
+
+variable "app_instances_prefix" {
+  type        = string
+  default     = "app"
+  description = "The name prefix of app instances."
+}
+
+variable "app_instances_size" {
+  type        = string
+  default     = "s-1vcpu-1gb"
+  description = "The size of app instances."
+}
+
+variable "app_instances_image" {
+  type        = string
+  default     = "centos-7-x64"
+  description = "The image of app instances."
+}
+
+variable "app_instances_tags" {
+  type        = list(string)
+  default     = ["app"]
+  description = "The tags of app instances."
+}
+
+variable "ssh_keys" {
+  type        = list(string)
+  default     = null
+  description = "The list of fingerprints of ssh public keys."
+}
+
+variable "db_instances_count" {
+  type        = number
+  default     = 0
+  description = "The total number of instances tagged by db."
+}
+
+variable "db_instances_prefix" {
+  type        = string
+  default     = "db"
+  description = "The name prefix of db instances."
+}
+
+variable "db_instances_size" {
+  type        = string
+  default     = "s-1vcpu-1gb"
+  description = "The size of db instances."
+}
+
+variable "db_instances_image" {
+  type        = string
+  default     = "centos-7-x64"
+  description = "The image of db instances."
+}
+
+variable "db_instances_tags" {
+  type        = list(string)
+  default     = ["db"]
+  description = "The tags of db instances."
+}
