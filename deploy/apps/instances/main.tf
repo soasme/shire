ecc4a9ffaa90e6067563444e8ca10b63e2b754f7
soasme <soasme@gmail.com>
2020-08-10 20:44:21 +1200

    init user_data for app instances.

--- deploy/apps/instances/main.tf
+++ deploy/apps/instances/main.tf
@@ -4,6 +4,32 @@ data "digitalocean_vpc" "this" {
   id = var.vpc_id
 }
 
+data "cloudinit_config" "app" {
+  gzip = false
+  base64_encode = false
+
+  part {
+    content_type = "text/x-shellscript"
+    filename = "bootstrap.sh"
+    content = <<EOT
+      yum update -y \
+        && yum install -y ansible \
+        && ansible-galaxy install geerlingguy.ntp cloudalchemy.node-exporter
+    EOT
+  }
+
+  part {
+    content_type = "text/x-shellscript"
+    filename = "init-server.sh"
+    content = <<EOT
+      /usr/local/bin/ansible-pull -U https://github.com/soasme/shire  \
+        -i 127.0.0.1, \
+        deploy/apps/instances/app.yml
+    EOT
+  }
+
+}
+
 resource "digitalocean_droplet" "app" {
   count              = var.app_instances_count
 
@@ -11,7 +37,8 @@ resource "digitalocean_droplet" "app" {
   size               = var.app_instances_size
   image              = var.app_instances_image
   tags               = var.app_instances_tags
-  region             = data.digitalocean_vpc.this.region
+  user_data          = local.app_user_data
+  region             = data.cloudinit_config.app.rendered
   ssh_keys           = var.ssh_keys
   vpc_uuid           = var.vpc_id
   private_networking = true
